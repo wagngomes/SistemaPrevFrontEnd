263e796707553d9cdbf8579176bc91bac90466d5
wagngomes <wagner.gomes.cx2@gmail.com>
2023-11-03 16:24:06 -0300

    paginação finalizada

--- front/src/pages/Previsao/Previsao.js
+++ front/src/pages/Previsao/Previsao.js
@@ -2,14 +2,24 @@ import './Previsao.css'
 import { FaSistrix, FaUserAlt } from 'react-icons/fa'
 import { useState, useEffect } from 'react'
 import NoteCard from '../../componentes/Card/NoteCard'
+import { Pagination } from '@mui/material'
+import { Box, CssBaseline, Container }from '@mui/material'
 
 const Previsao = () => {
     const [dadosCard, setDadosCard] = useState([])
+    const [page, setPage] = useState(1)
+    const [totalPages, setTotalPages] = useState(1)
+    const cardsPerPage = 6
+
+    const handlePageChange = (event, newPage) => {
+        setPage(newPage);
+        // Execute ação de buscar dados com a nova página
+    };
 
 
     useEffect(() => {
         buscaRequisicoes()
-    }, [])
+    }, [page])
 
     async function buscaRequisicoes() {
 
@@ -17,16 +27,19 @@ const Previsao = () => {
         const _token = localStorage.getItem('token')
         var solicitante_email = email.replace(/^"(.*)"$/,'$1')
         try {
-            const todasAsRequisicoesEmAberto = await fetch(`http://localhost:5000/previsoes/${solicitante_email}`, {
+            const todasAsRequisicoesEmAberto = await fetch(`http://localhost:5000/previsoes/${solicitante_email}?page=${page}&limit=${cardsPerPage}`, {
                 method: 'GET',
                 headers: {
                     'Authorization': `Bearer ${_token}`
                 }
             })
             const dadosConvertidos = await todasAsRequisicoesEmAberto.json()
-            var cod = dadosConvertidos[0].codigo
+            const totalPageCount = dadosConvertidos.count
 
-            setDadosCard(dadosConvertidos)
+            setDadosCard(dadosConvertidos.rows)
+            setPage(page);
+            setTotalPages(totalPageCount)
+            console.log(dadosConvertidos)
         } catch (erro) {
             
         }
@@ -48,6 +61,9 @@ const Previsao = () => {
                 <h3>Área de previsões</h3>
             </section>
             <main>
+         
+
+                
             {dadosCard.map((dadosReq, index) => (
                     <NoteCard
                     key={index}
@@ -61,9 +77,17 @@ const Previsao = () => {
                   
                 ))}
 
-
+           
             </main>
 
+            <Box display="flex" justifyContent="center">
+            <Pagination 
+                    count={totalPages}
+                    defaultPage={page}
+                    onChange={handlePageChange}
+                    size='small'
+                />
+            </Box>
         </div>
     )
 
